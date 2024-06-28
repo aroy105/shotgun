d8fd24089b1545462d9b21381daca889dccfaa33
aroy105 <orghois@gmail.com>
2024-06-28 17:09:36 -0400

    kuhn poker scaffolding

---
+++ simple_cfr_poker_solver/game/kuhn_poker.py
@@ -0,0 +1,14 @@
+def is_terminal(history):
+    pass
+
+def payoff(history, player_card, opponent_card):
+    pass
+
+def available_actions(history):
+    pass
+
+def get_current_player(history):
+    pass
+
+def build_info_set(player_card, history):
+    return player_card + history
