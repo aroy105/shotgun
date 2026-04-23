930acbfe3e305803ed01827832db311c5fb65926
aroy105 <orghois@gmail.com>
2026-04-22 23:58:27 -0400

    add node struct for CFR regret + get_strategy function

--- include/types.hpp
+++ include/types.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <algorithm>
+#include <array>
 #include <string>
 
 namespace shotgun::kuhn {
@@ -21,6 +23,30 @@ struct State {
     Card p1_card;
     Card p2_card;
     std::string history;
+};
+
+struct Node {
+    std::array<double, 2> regret_sum{};
+    std::array<double, 2> strategy_sum{};
+
+    std::array<double, 2> get_strategy() const {
+        std::array<double, 2> strategy{
+            std::max(0.0, regret_sum[0]),
+            std::max(0.0, regret_sum[1]),
+        };
+
+        double normalizing_sum = strategy[0] + strategy[1];
+
+        if (normalizing_sum > 0.0) {
+            strategy[0] /= normalizing_sum;
+            strategy[1] /= normalizing_sum;
+        } else {
+            strategy[0] = 0.5;
+            strategy[1] = 0.5;
+        }
+
+        return strategy;
+    }
 };
 
 }
