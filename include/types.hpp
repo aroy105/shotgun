a09440d5fe0cee6d363b77229fd1b670a93461a1
aroy105 <orghois@gmail.com>
2026-04-24 21:31:33 -0400

    added get_average_strategy function to get what the average infoset strategy is after all our iterations

--- include/types.hpp
+++ include/types.hpp
@@ -47,6 +47,25 @@ struct Node {
 
         return strategy;
     }
+
+    std::array<double, 2> get_average_strategy() const {
+        std::array<double, 2> avg{
+            strategy_sum[0],
+            strategy_sum[1]
+        };
+
+        double normalizing_sum = avg[0] + avg[1];
+
+        if (normalizing_sum > 0.0) {
+            avg[0] /= normalizing_sum;
+            avg[1] /= normalizing_sum;
+        } else {
+            avg[0] = 0.5;
+            avg[1] = 0.5;
+        }
+
+        return avg;
+    }
 };
 
 }
